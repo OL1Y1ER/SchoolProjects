166456e3dc7dace7106f1749c9a3c2570c322d22
OL1Y1ER <134446761+OL1Y1ER@users.noreply.github.com>
2024-01-05 10:45:31 +0100

    Base structure implimented

--- cesar.cpp
+++ cesar.cpp
@@ -2,11 +2,29 @@
 #include <string>
 int main(){
   
-  string alphabets = "abcdefghijklmnopqrstuvwxyz";
+  std::string alpha = "abcdefghijklmnopqrstuvwxyz";
 
-  string str;
-  std::cin >> str;
+  std::cout << "Put in value: ";
 
+  int change = 0;
+  std::cin >> change;
+
+  std::cout << std::endl << "input: ";
+  std::string input;
+  std::cin >> input;
+
+
+  for(int i =0; i<input.length(); i++){
+    
+    for(int j = 0; j<alpha.length(); j++){
+      if(input[i] == alpha[j]){
+        std::cout << alpha[(i+change)%alpha.length()];
+        
+      }
+    }
+
+
+  }
 
   
   
